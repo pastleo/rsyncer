a80c67f79d33072011a426eb52ec9282a76bc03c
PastLeo <chgu82837@gmail.com>
2017-09-13 01:13:55 +0800

    add time logging in each step

--- run.sh
+++ run.sh
@@ -22,22 +22,24 @@ log_file="${DIR}/run.log"
 mv "$log_file" "${log_file}.last"
 set -e
 
-date >> "$log_file"
-echo '' >> "$log_file"
+echo '<< rsyncer >>' >> "$log_file"
 
 echo '========================================' >> "$log_file"
+date >> "$log_file"
 echo '>> patch -> src (update):' >> "$log_file"
 echo rsync -rtWLKhv --inplace --size-only --prune-empty-dirs --include='/.gitignore' --exclude '.*/' --exclude '.*' "${DIR}/patch/" "${DIR}/src/" >> "$log_file"
 rsync -rtWLKhv --inplace --size-only --prune-empty-dirs --include='/.gitignore' --exclude '.*/' --exclude '.*' "${DIR}/patch/" "${DIR}/src/" >> "$log_file"
 echo '' >> "$log_file"
 
 echo '========================================' >> "$log_file"
+date >> "$log_file"
 echo '>> src -> des (sync):' >> "$log_file"
 echo rsync -rtWLKhv --inplace --size-only --del --delete-excluded --include='/.gitignore' --exclude '.*/' --exclude '.*' "${DIR}/src/" "${DIR}/des/" >> "$log_file"
 rsync -rtWLKhv --inplace --size-only --del --delete-excluded --include='/.gitignore' --exclude '.*/' --exclude '.*' "${DIR}/src/" "${DIR}/des/" >> "$log_file"
 echo '' >> "$log_file"
 
 echo '========================================' >> "$log_file"
+date >> "$log_file"
 echo '>> src -> patch (blank folder structure):' >> "$log_file"
 echo rsync -rtWLKhv --inplace --size-only --del --delete-excluded --include='/.gitignore' -f'+ */' -f'- *' --exclude '.*/' --exclude '.*' "${DIR}/src/" "${DIR}/patch/" >> "$log_file"
 rsync -rtWLKhv --inplace --size-only --del --delete-excluded --include='/.gitignore' -f'+ */' -f'- *' --exclude '.*/' --exclude '.*' "${DIR}/src/" "${DIR}/patch/" >> "$log_file"
